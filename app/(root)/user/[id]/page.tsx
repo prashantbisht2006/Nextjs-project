1456364318206b00b95afad2da3bdc1e63588a59
Prashant Bisht <prashantbisht284@gmail.com>
2025-05-31 00:55:26 +0530

    user profile

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -21,7 +21,38 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   return (
     <>
-      
+      <section className="profile_container "> 
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center line-clamp-1">
+              {user.name}
+            </h3>
+          </div>
+          <Image
+          src={user.image}
+          alt={user.name}
+          width={220}
+          height={220}
+          className="profile_image"/>
+
+          <p className="text-20-extrabold mt text-center">
+            @{user.username}
+          </p>
+          <p className="mt-1 text-center text-14-normal">
+            {user?.bio ||"No bio available"}
+          </p>
+        </div>
+        <div className="flex-1 flex-col w-full max-w-7xl mx-auto gap-5 lg:mt-5">
+          <p className="text-30-bold">
+            {session?.user?.name === user.name ? "Your" : "All"} Startup's
+          </p>
+          <ul className="card_grid-sm">
+            {/* todooooooooo */}
+
+          </ul>
+
+        </div>
+      </section>
           
        
     </>
